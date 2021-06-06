d2b918b69586070f19146a4dbfa56bdaa2ee6141
Fran Lara <francesc.lara@hotmail.com>
2021-06-06 20:45:59 +0200

    done

--- src/movies.js
+++ src/movies.js
@@ -10,29 +10,78 @@ function getAllDirectors(movies) {
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(movies) {
- const manyMovies = movies.filter(
+  /* const newArray = movies.filter((directorName) =>{
+    return directorName.director === "Steven Spielberg" && directorName.genre.includes("Drama") ;
+  })
+  return newArray.length;
+  */
+
+  const manyMovies = movies.filter(
     ({ director, genre }) =>
       genre.includes("Drama") && director === "Steven Spielberg").length;
   return manyMovies;
 }
 
-
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(arrAve) {
-  let total = arrAve.reduce((acc,el)=>{
-
-  })  
+ if (arrAve.length === 0) return 0; 
+ else{
+   let total = arrAve.reduce((acc,movie) => {
+     if(movie.score){
+       const updatedAcc = acc + movie.score;
+       return updatedAcc;
+     }
+     else{
+       return acc;
+     }
+   }, 0);
+   return Number((total / arrAve.length).toFixed(2));
+ }
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(movies) {
+  if (!movies.length){
+    return 0;
+  }
+  else{
+    let dramaMovies = movies.filter((movie) => movie.genre.includes('Drama'));
+    return scoresAverage(dramaMovies);
+  }
+
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  const moviesCopy = movies.map((movie) => {
+    return movie;
+  });
+
+  moviesCopy.sort(function(a,b){
+    if (a.year > b.year){
+      return 1;
+    } else if (a.year < b.year){
+      return -1;
+    } else {
+      if(a.title < b.title){
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  });
+  return moviesCopy;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(movies) {
+  const moviesCopy = movies.map((movie) =>{
+    return movie;
+  });
+
+  const orderesMovies = moviesCopy.sort((a,b) => a.title.localeCompare(b.title)).map((movie) =>movie.title).slice(0,20);
+  return orderesMovies;
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
